d70c433361558bd7ed3df6ce77eaa8a10dce3b29
Li Zhichuan <cristianoli@163.com>
2018-11-20 18:57:46 +0800

    Add outer join

--- MySQL/MySQL_Crash_Couse.sql
+++ MySQL/MySQL_Crash_Couse.sql
@@ -109,13 +109,20 @@ where prod_price <= 5
 union
 select vend_id, prod_id, prod_price
 from products
-where vend_id in (1001, 1002);
+where vend_id in (1001, 1002) order by vend_id, prod_id;
 
 -- 上面的union语句其实用or也可以 --
 select vend_id, prod_id, prod_price
 from products
 where prod_price <= 5 or vend_id in (1001, 1002);
 
+select vend_id, prod_id, prod_price
+from products
+where prod_price <= 5
+union all
+select vend_id, prod_id, prod_price
+from products
+where vend_id in (1001, 1002) order by vend_id, prod_id;
 
 -- create view and use view--
 create view productcustomer as
